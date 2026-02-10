77cca357e0d6c4c18bc40073d139b28d0d559d95
Krishna-zz <krishnagunjal022@gmail.com>
2026-02-10 18:18:21 +0530

    making frontend

--- Client/AI-chatbot/src/Chatbox.jsx
+++ Client/AI-chatbot/src/Chatbox.jsx
@@ -1,17 +1,33 @@
 import { useState } from "react";
+import ollama from "ollama"
 
-
-function Chatbox(){
+function Chatbox (){
 
    const [input, setInput] = useState("");
    const [messages, setMessages] = useState([])
 
 
-   
+   const send = async() => {
+
+    if (!input) return;
+
+    setMessages([...messages, {role: "You", text: input}])
+
+    const res = await fetch("http://localhost:3000/chat" , {
+
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({message: input})
+    })
+
+    const data = await res.json();
+
+    setMessages(m => [...m, {role: "Krishna", text: data.reply}])
+   }
 
 
   return (
-      <div>Krishna</div>
+      <div className="bg-amber-500">Krishna</div>
   );
 }
 
